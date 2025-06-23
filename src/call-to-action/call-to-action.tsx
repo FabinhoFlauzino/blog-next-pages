2d7e4424bb69627c18e1bc8b771877124f1fab88
Fabio Flauzino <fabioflauzino@hotmail.com>
2025-06-23 12:49:16 -0300

    Refatorando Imagens

--- src/call-to-action/call-to-action.tsx
+++ src/call-to-action/call-to-action.tsx
@@ -4,16 +4,16 @@ import Link from 'next/link';
 
 export const CallToAction = () => {
   return (
-    <section className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
-      <div className="container">
+    <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div className="absolute inset-0 bg-[url('/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
+
+      <div className="container relative">
         <div className="flex flex-col items-center gap-6 text-center">
-          <div className="bg-cyan-300 rounded-full" style={{padding: "16px"}}>
+          <div className="p-4 bg-cyan-300 w-fit rounded-full">
             <Store className="text-cyan-100" />
           </div>
 
-          <h2
-            className={`font-sans text-gray-100 text-balance text-heading-xl max-w-lg`}
-          >
+          <h2 className="font-sans text-gray-100 text-balance text-heading-xl">
             Crie uma loja online e inicie suas vendas ainda hoje
           </h2>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { CallToAction } from "@/call-to-action";
+import { CallToAction } from "@/call-to-action/call-to-action";
 import { CustomerStorySection } from "@/components/customer-story-section";
 import { FeatureSection } from "@/components/feature-section";
 import { HeroSection } from "@/components/hero-section";
