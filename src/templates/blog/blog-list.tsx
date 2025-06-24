6fde9b300234a469b8b086cf4cfcf46bf979154b
Fabio Flauzino <fabioflauzino@hotmail.com>
2025-06-24 12:38:37 -0300

    Criando o grid de card

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,6 +1,7 @@
 import { Search } from "@/components/search";
 import { useRouter } from "next/router";
 import { PostCard } from "./components/post-card";
+import { PostGridCard } from "./components/post-grid-card";
 
 export function BlogList() {
   const router = useRouter()
@@ -8,24 +9,40 @@ export function BlogList() {
 
   const pageTItle = query ? `Resultados de busca para "${query}"` : "Transformando seu negócio em uma loja virtual"
 
-  return <div className="flex flex-col py-24 flex-grow h-full">
-    <header>
-      <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
+  return (
+    <div className="flex flex-col py-24 flex-grow h-full">
+      <header className="pb-14">
+        <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
 
-        <div className="flex flex-col gap-4 md:px-0">
-          {/**TAG */}
-          <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">BLOG</span>
+          <div className="flex flex-col gap-4 md:px-0">
+            {/**TAG */}
+            <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">BLOG</span>
 
-          {/**Titulo */}
-          <h1 className="text-balance text-start md:text-left text-heading-lg md:text-heading-xl max-w-2xl text-gray-100">{pageTItle}</h1>
+            {/**Titulo */}
+            <h1 className="text-balance text-start md:text-left text-heading-lg md:text-heading-xl max-w-2xl text-gray-100">{pageTItle}</h1>
+          </div>
+          {/**Search */}
+          <Search />
         </div>
-        {/**Search */}
-        <Search />
-      </div>
-    </header>
+      </header>
 
 
-    {/**Listagem de post */}
-    <PostCard />
-  </div>;
+      {/**Listagem de post */}
+      <PostGridCard>
+
+        <PostCard
+          title="Transformando seu negócio em uma loja virtual"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
+          date="20/12/2024"
+          slug="Transformando"
+          image="/assets/primeiro-post.png"
+          author={{
+            name: "João da Silva",
+            avatar: "/customer-01.png"
+          }}
+        />
+
+      </PostGridCard>
+    </div>
+  )
 }
