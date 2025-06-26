2a76aa515131a7961d683b67e7bf2abc8dae8a50
Fabio Flauzino <fabioflauzino@hotmail.com>
2025-06-26 18:17:08 -0300

    Aplicando SSG no blog

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,7 +1,25 @@
-import { BlogList } from "@/templates/blog";
+import { BlogList, type BlogListProps } from "@/templates/blog";
+import { allPosts } from "contentlayer/generated";
+import { GetStaticProps } from "next";
 
-export default function BlogPage() {
+export default function BlogPage({posts}: BlogListProps) {
+  console.log(posts)
   return (
-    <BlogList />
+    <BlogList posts={posts}/>
   )
 }
+
+export const getStaticProps = (async () => {
+  const sortedPosts = allPosts.sort((a, b) => {
+    if (new Date(a.date) > new Date(b.date)) {
+      return -1;
+    }
+    return 1;
+  });
+
+  return {
+    props: {
+      posts: sortedPosts
+    },
+  }
+}) satisfies GetStaticProps<BlogListProps>
