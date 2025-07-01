c6392592ad57660207d00c28d8896d5ae260be15
Fabio Flauzino <fabioflauzino@hotmail.com>
2025-07-01 15:25:26 -0300

    Add SEO em Landingpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
 import { LandingPage } from "@/templates/landing-page";
+import { Metadata } from "next";
+
+export const metadada: Metadata = {
+  title: "Site.Set",
+  description: "Venda seus produtos",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.Set",
+    description: "Venda seus produtos",
+    url: "https://blog-next-pages.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://blog-next-pages.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ]
+  }
+}
 
 export default function HomePage() {
   return (
