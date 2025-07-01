ecbe0ef3214919a66acf6ac6f9067b9e6a17de65
Fabio Flauzino <fabioflauzino@hotmail.com>
2025-07-01 17:13:02 -0300

    Add SEO em BlogPage

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,28 @@
-import { BlogList } from "@/templates/blog";
+import type { Metadata } from "next";
 import { allPosts } from "contentlayer/generated";
+import { BlogList } from "@/templates/blog";
 
+export const metadata: Metadata = {
+  title: 'Site.Set Blog',
+  description: 'Dicas e estratégias para impulsionar seu negócio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set Blog',
+    description: 'Dicas e estratégias para impulsionar seu negócio',
+    url: 'https://nextjs-fundamentos.vercel.app/og-image.jpg',
+    siteName: 'Site.Set Blog',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://nextjs-fundamentos.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set Blog',
+      },
+    ],
+  },
+};
 export default function BlogListPage() {
   const sortedPost = allPosts.sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
